f988ac7d630f80ecbee21b209af18b5c57f0a3e9
Manic-sh <mash.pro666@gmail.com>
2023-09-20 13:51:01 +0530

    Package fixes

--- src/context/HeaderContext.tsx
+++ src/context/HeaderContext.tsx
@@ -1,1 +1,33 @@
+import React, { createContext, useContext, useState } from "react";
 
+// Define the type for the context
+interface HeaderVisibilityContextType {
+  showHeader: boolean;
+  setShowHeader: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const HeaderVisibilityContext = createContext<
+  HeaderVisibilityContextType | undefined
+>(undefined);
+
+export const HeaderVisibilityProvider: React.FC<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  const [showHeader, setShowHeader] = useState<boolean>(true); // Default to true, change as needed
+
+  return (
+    <HeaderVisibilityContext.Provider value={{ showHeader, setShowHeader }}>
+      {children}
+    </HeaderVisibilityContext.Provider>
+  );
+};
+
+export function useHeaderVisibility(): HeaderVisibilityContextType {
+  const context = useContext(HeaderVisibilityContext);
+  if (context === undefined) {
+    throw new Error(
+      "useHeaderVisibility must be used within a HeaderVisibilityProvider"
+    );
+  }
+  return context;
+}
